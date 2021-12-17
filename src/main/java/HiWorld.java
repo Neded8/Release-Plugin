69b0e3a37620de70425ee34850a9202bbbbfc474
vladislavo <vlad.opanasiuk.99@gmail.com>
2021-12-17 10:26:30 +0200

    First commit

---
+++ src/main/java/HiWorld.java
@@ -0,0 +1,6 @@
+public class HiWorld {
+    public static void main(String args[]){
+        System.out.println("Maven-RELEASE_PLUGIN");
+        System.out.println("Version: " + HiWorld.class.getPackage().getImplementationVersion());
+    }
+}
